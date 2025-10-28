490e796090c18dd90ea8fb9a86f7f98356c4a643
Teymur <azimovta@gmail.com>
2025-10-28 12:28:52 +0500

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -8,9 +8,55 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class Search {
+
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".js")).forEach(System.out::println);
+
+        validate(args);
+        Path start = Paths.get(args[0]);
+        search(start, path -> path.toFile().getName().endsWith(args[1]))
+                .forEach(System.out::println);
+    }
+
+    private static void validate(String[] args) {
+        Path dir = Paths.get(args[0]);
+        if (args.length != 2) {
+            throw new IllegalArgumentException(""" 
+                    There should be exactly 2 parameters.
+                    
+                    Usage example:
+                        1st parameter: args[0] - the search directory path.
+                        2nd parameter: args[1] - the file extension (e.g., .js, .txt).
+                    """);
+        }
+
+        if (!Files.exists(dir)) {
+            throw new IllegalArgumentException("""
+                    The first parameter must be an existing directory path.
+                    
+                    Usage example:
+                        args[0] - search directory path (e.g., C:\\projects\\job4j_design)
+                        args[1] - file extension (e.g., .js, .txt)
+                    """);
+        }
+
+        if (!Files.isDirectory(dir)) {
+            throw new IllegalArgumentException("""
+                    The first parameter should be a directory, not a file.
+                    
+                    Usage example:
+                        args[0] - path to the search directory (e.g., C:\\projects\\job4j_design)
+                        args[1] - file extension (e.g., .js, .txt)
+                    """);
+        }
+
+        if (!(args[1]).startsWith(".")) {
+            throw new IllegalArgumentException("""
+                    The second parameter must be a file extension starting with a dot:
+                    
+                    Usage example:
+                        args[1] - file extension (e.g., .js, .txt)
+                    """);
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
